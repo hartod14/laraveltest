bdc0807977e7cb19bc8b8736b74fa107485b88a3
LAPTOP-KAAFL62K\User <haritody@gmail.com>
2022-07-17 21:03:16 +0700

    malam

--- app/Http/Controllers/DashboardPostController.php
+++ app/Http/Controllers/DashboardPostController.php
@@ -12,9 +12,12 @@ class DashboardPostController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
+
     public function index()
     {
-        //
+        return view('dashboard.posts.index',[
+            'posts' => Post::where('user_id',auth()->user()->id)->get()
+        ]);
     }
 
     /**
@@ -46,7 +49,9 @@ class DashboardPostController extends Controller
      */
     public function show(Post $post)
     {
-        //
+        return view('dashboard.posts/show' , [
+            'post' => $post
+        ]);
     }
 
     /**
